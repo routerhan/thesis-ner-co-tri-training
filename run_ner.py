ccbcb1d30e53902e8adb2716c1bdf03c08842e13
chinchen <chinchen@chinchens-MacBook-Pro.local>
2020-04-19 11:23:50 +0200

    Try to set up the order of class
    Remove the sorted label

--- run_ner.py
+++ run_ner.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os
 import random
+import re
 
 import numpy as np
 import torch
@@ -18,7 +19,7 @@ from torch.utils.data.distributed import DistributedSampler
 from tqdm import tqdm, trange
 from preprocessor import IswPreprocessor, convert_examples_to_features, InputFeatures
 from utils import split_data
-
+from sklearn.metrics import classification_report
 from seqeval.metrics import classification_report
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
@@ -206,41 +207,41 @@ def main():
 
     if args.local_rank not in [-1, 0]:
         torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
+    if args.do_train:
+        # Prepare initialized model
+        config = BertConfig.from_pretrained(args.bert_model, num_labels=num_labels, finetuning_task=args.task_name)
+        model = Ner.from_pretrained(args.bert_model,
+                from_tf = False,
+                config = config)
 
-    # Prepare model
-    config = BertConfig.from_pretrained(args.bert_model, finetuning_task=args.task_name)
-    model = Ner.from_pretrained(args.bert_model,
-              from_tf = False,
-              config = config)
+        if args.local_rank == 0:
+            torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
 
-    if args.local_rank == 0:
-        torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
+        model.to(device)
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias','LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': args.weight_decay},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            ]
+        warmup_steps = int(args.warmup_proportion * num_train_optimization_steps)
+        optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+        scheduler = WarmupLinearSchedule(optimizer, warmup_steps=warmup_steps, t_total=num_train_optimization_steps)
+        if args.fp16:
+            try:
+                from apex import amp
+            except ImportError:
+                raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+            model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
 
-    model.to(device)
-    param_optimizer = list(model.named_parameters())
-    no_decay = ['bias','LayerNorm.weight']
-    optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': args.weight_decay},
-        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-        ]
-    warmup_steps = int(args.warmup_proportion * num_train_optimization_steps)
-    optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
-    scheduler = WarmupLinearSchedule(optimizer, warmup_steps=warmup_steps, t_total=num_train_optimization_steps)
-    if args.fp16:
-        try:
-            from apex import amp
-        except ImportError:
-            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
-        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
+        # multi-gpu training (should be after apex fp16 initialization)
+        if n_gpu > 1:
+            model = torch.nn.DataParallel(model)
 
-    # multi-gpu training (should be after apex fp16 initialization)
-    if n_gpu > 1:
-        model = torch.nn.DataParallel(model)
-
-    if args.local_rank != -1:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
-                                                          output_device=args.local_rank,
-                                                          find_unused_parameters=True)
+        if args.local_rank != -1:
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
+                                                            output_device=args.local_rank,
+                                                            find_unused_parameters=True)
 
     global_step = 0
     nb_tr_steps = 0
@@ -332,9 +333,6 @@ def main():
             eval_sentences = joblib.load('data/dev-isw-sentences.pkl')
             eval_labels = joblib.load('data/dev-isw-labels.pkl')
             # TO check : label_list should be the whole list ? => load label list from model_config 
-            with open('model_config.json') as f:
-                config = json.load(f)
-            label_list = [label for label in config['label_map'].values()]
             eval_label_list = label_list
         elif args.eval_on == "test":
             eval_sentences = joblib.load('data/test-isw-sentences.pkl')
@@ -396,8 +394,8 @@ def main():
                     else:
                         temp_1.append(label_map[label_ids[i][j]])
                         temp_2.append(label_map[logits[i][j]])
-
-        report = classification_report(y_true, y_pred,digits=4)
+        
+        report = classification_report(y_true, y_pred, digits=4)
         logger.info("\n%s", report)
         output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
         with open(output_eval_file, "w") as writer:
