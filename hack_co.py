bc22c41c2e5af954a7d3163d12f2ed762fe946ba
routerhan <he5540192@gmail.com>
2020-07-09 22:18:43 +0200

    Add evaluation process and saved trained models

--- hack_co.py
+++ hack_co.py
@@ -26,10 +26,21 @@ def main():
         k_ls = [500, 1000, 1500, 2000, 2500]
         for k in k_ls:
             logger.info(" ***** Iteration k : {} ***** ".format(k))
-            ext_output_dir = "ext_data/ext_k_{}".format(k)
-            script = "python run_cotrain.py --ext_output_dir {} --modelA_dir baseline_model --modelB_dir onto_model --de_unlabel_dir machine_translation/2017_de_sents.txt --en_unlabel_dir machine_translation/2017_en_sents.txt --k {} --u 200 --top_n 10 --save_preds --save_agree".format(ext_output_dir, k)
+            ext_output_data_dir = "ext_data/ext_k_{}".format(k)
+            logger.info(" ***** Selecting ext_data ***** ")
+            script = "python run_cotrain.py --ext_output_dir {} --modelA_dir baseline_model --modelB_dir onto_model --de_unlabel_dir machine_translation/2017_de_sents.txt --en_unlabel_dir machine_translation/2017_en_sents.txt --k {} --u 200 --top_n 10 --save_preds --save_agree".format(ext_output_data_dir, k)
             os.system(script)
-            logger.info(" ***** Ext model saved in : {} ***** ".format(ext_output_dir))
+            logger.info(" ***** Ext data saved in : {} ***** ".format(ext_output_data_dir))
+
+            logger.info(" ***** Start re-train on ext_data ***** ")
+            ext_output_model_dir = "co-models/ext_k_{}_model/".format(k)
+            script = "python run_ner.py --max_seq_length 128 --do_train --extend_L --ext_data_dir {} --ext_output_dir {}".format(ext_output_data_dir, ext_output_model_dir)
+            os.system(script)
+            logger.info(" ***** Ext model saved in : {} ***** ".format(ext_output_model_dir))
+
+            logger.info(" ***** Evaluate new re-train model ***** ")
+            script = "python run_ner.py --max_seq_length 128 --do_eval --eval_on test --extend_L --ext_output_dir {}".format(ext_output_model_dir)
+            os.system(script)
 
 if __name__ == '__main__':
     main()
