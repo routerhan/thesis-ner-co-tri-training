6b5e4a5871d0167297032461fffb0c1515f7ab6c
routerhan <he5540192@gmail.com>
2020-07-10 23:30:26 +0200

    Add tuning hack pipeline

--- hack_co.py
+++ hack_co.py
@@ -51,5 +51,59 @@ def main():
                 script = "python run_ner.py --max_seq_length 128 --do_eval --eval_on test --extend_L --ext_output_dir {}".format(ext_output_model_dir)
                 os.system(script)
 
+    if args.tune_top_n:
+        logger.info(" ***** Start Co-training, tuning top_n ***** ")
+        t_ls = [5, 10, 15, 20, 25]
+        for t in t_ls:
+            logger.info(" ***** Top_N: {} ***** ".format(t))
+            ext_output_data_dir = "ext_data/ext_top_{}".format(t)
+            if os.path.exists(ext_output_data_dir) and os.listdir(ext_output_data_dir):
+                pass
+            else:
+                logger.info(" ***** Selecting ext_data ***** ")
+                script = "python run_cotrain.py --ext_output_dir {} --modelA_dir baseline_model --modelB_dir onto_model --de_unlabel_dir machine_translation/2017_de_sents.txt --en_unlabel_dir machine_translation/2017_en_sents.txt --k 1000 --u 200 --top_n {} --save_preds --save_agree".format(ext_output_data_dir, t)
+                os.system(script)
+                logger.info(" ***** Ext data saved in : {} ***** ".format(ext_output_data_dir))
+
+        if args.do_retrain:
+            for t in t_ls:
+                logger.info(" ***** Start re-train on ext_data ***** ")
+                ext_output_model_dir = "co-models/ext_top_{}_model/".format(t)
+                ext_output_data_dir = "ext_data/ext_top_{}".format(t)
+                script = "python run_ner.py --max_seq_length 128 --do_train --extend_L --ext_data_dir {} --ext_output_dir {}".format(ext_output_data_dir, ext_output_model_dir)
+                os.system(script)
+                logger.info(" ***** Ext model saved in : {} ***** ".format(ext_output_model_dir))
+
+                logger.info(" ***** Evaluate new re-train model ***** ")
+                script = "python run_ner.py --max_seq_length 128 --do_eval --eval_on test --extend_L --ext_output_dir {}".format(ext_output_model_dir)
+                os.system(script)
+
+    if args.tune_pool_value:
+        logger.info(" ***** Start Co-training, tuning tune_pool_value u ***** ")
+        u_ls = [50, 100, 150, 200, 250]
+        for u in u_ls:
+            logger.info(" ***** Pool_u: {} ***** ".format(u))
+            ext_output_data_dir = "ext_data/ext_u_{}".format(u)
+            if os.path.exists(ext_output_data_dir) and os.listdir(ext_output_data_dir):
+                pass
+            else:
+                logger.info(" ***** Selecting ext_data ***** ")
+                script = "python run_cotrain.py --ext_output_dir {} --modelA_dir baseline_model --modelB_dir onto_model --de_unlabel_dir machine_translation/2017_de_sents.txt --en_unlabel_dir machine_translation/2017_en_sents.txt --k 1000 --u {} --top_n 10 --save_preds --save_agree".format(ext_output_data_dir, u)
+                os.system(script)
+                logger.info(" ***** Ext data saved in : {} ***** ".format(ext_output_data_dir))
+
+        if args.do_retrain:
+            for u in u_ls:
+                logger.info(" ***** Start re-train on ext_data ***** ")
+                ext_output_model_dir = "co-models/ext_u_{}_model/".format(u)
+                ext_output_data_dir = "ext_data/ext_u_{}".format(u)
+                script = "python run_ner.py --max_seq_length 128 --do_train --extend_L --ext_data_dir {} --ext_output_dir {}".format(ext_output_data_dir, ext_output_model_dir)
+                os.system(script)
+                logger.info(" ***** Ext model saved in : {} ***** ".format(ext_output_model_dir))
+
+                logger.info(" ***** Evaluate new re-train model ***** ")
+                script = "python run_ner.py --max_seq_length 128 --do_eval --eval_on test --extend_L --ext_output_dir {}".format(ext_output_model_dir)
+                os.system(script)
+
 if __name__ == '__main__':
     main()
